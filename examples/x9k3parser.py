1e10fe5aefa3af6a117d34a5cc9944a1467d5a4b
Adrian <52701496+futzu@users.noreply.github.com>
2022-04-03 19:47:07 -0400

    Create x9k3parser.py

---
+++ examples/x9k3parser.py
@@ -0,0 +1,154 @@
+"""
+x9k3parser
+"""
+
+
+import os
+import sys
+import pyaes
+import threefive
+
+
+class Stanza:
+    """
+    The Stanza class represents a segment
+    and associated data
+
+    """
+
+    def __init__(self, lines, segment, start):
+        self.lines = lines
+        self.segment = segment
+        self.clean_segment()
+        self.pts = None
+        self.start = start
+        self.duration = 0
+        self.cue = False
+
+    def clean_segment(self):
+        if self.segment.startswith("http"):
+            ss = self.segment.split("/")
+            while ".." in ss:
+                i = ss.index("..")
+                del ss[i]
+                del ss[i - 1]
+            self.segment = "/".join(ss)
+
+    def _get_pts_start(self, seg):
+        if not self.start:
+            pts_start = 0.0
+            try:
+                strm = threefive.Stream(seg)
+                strm.decode(func=None)
+                end = round(strm._prgm_pts[1] / 90000.0, 6)
+
+                if len(strm.start.values()) > 0:
+                    pts_start = strm.start.popitem()[1]
+                self.pts = round(pts_start / 90000.0, 6)
+            except:
+                pass
+        self.start = self.pts
+
+    def _extinf(self, line):
+        if line.startswith("#EXTINF"):
+            t = line.split(":")[1].split(",")[0]
+            t = float(t)
+            self.duration = round(t, 6)
+
+    def _ext_x_scte35(self, line):
+        if line.startswith("#EXT-X-SCTE35"):
+            self.cue = line.split("CUE=")[1].split(",")[0]
+            self.do_cue()
+
+    def do_cue(self):
+        """
+        do_cue parses a SCTE-35 encoded string
+        via the threefive.Cue class
+        """
+        if self.cue:
+            tf = threefive.Cue(self.cue)
+            tf.decode()
+            tf.show()
+
+    def decode(self):
+        for line in self.lines:
+            print(line)
+            self._ext_x_scte35(line)
+            self._extinf(line)
+            if not self.pts:
+                self._get_pts_start(self.segment)
+                self.start = self.pts
+        if not self.start:
+            self.start = 0.0
+        return self.start
+
+
+class X9K3Parser:
+    """
+    X9K3 Parser for x9k3 generated
+    Manifests.
+    """
+
+    def __init__(self, arg):
+        self.m3u8 = arg
+        self.hls_time = 0.0
+        self.seg_list = []
+        self._start = None
+        self.chunk = []
+        self.base_uri = ""
+        if arg.startswith("http"):
+            self.base_uri = arg[: arg.rindex("/") + 1]
+        self.manifest = None
+        self.next_expected = 0
+
+    @staticmethod
+    def _clean_line(line):
+        if isinstance(line, bytes):
+            line = line.decode(errors="ignore")
+        line = (
+            line.replace(" ", "").replace('"', "").replace("\n", "").replace("\r", "")
+        )
+        return line
+
+    def show_segment_times(self, stanza):
+        print(f"Segment Start: {round(stanza.start,6)}")
+        print(f"Segment Duration: {stanza.duration}")
+        print(f"HLS Time: {round(self.hls_time,6)}")
+
+    def do_segment(self, line):
+        segment = line
+        if not line.startswith("http"):
+            segment = self.base_uri + line
+        if segment not in self.seg_list:
+            self.seg_list.append(segment)
+            self.seg_list = self.seg_list[-200:]
+            stanza = Stanza(self.chunk, segment, self._start)
+            stanza.decode()
+            if not self._start:
+                self._start = stanza.start
+            self.next_expected = self._start + self.hls_time
+            self.next_expected += round(stanza.duration, 6)
+            self.hls_time += stanza.duration
+            self.show_segment_times(stanza)
+
+        self.chunk = []
+
+    def decode(self):
+        while True:
+            with threefive.reader(self.m3u8) as self.manifest:
+                while self.manifest:
+                    line = self.manifest.readline()
+                    if not line:
+                        break
+                    line = self._clean_line(line)
+                    if "ENDLIST" in line:
+                        return False
+                    self.chunk.append(line)
+                    if not line.startswith("#"):
+                        self.do_segment(line)
+                        print("\n")
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    X9K3Parser(arg).decode()
