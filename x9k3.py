55c67916ea824a609d9456039701730db9b1c534
Adrian <52701496+futzu@users.noreply.github.com>
2022-05-19 11:43:12 -0400

    added version and version_number

--- x9k3.py
+++ x9k3.py
@@ -11,6 +11,36 @@ from base64 import b64encode
 from functools import partial
 from threefive import Stream
 
+"""
+Odd number versions are releases.
+Even number versions are testing builds between releases.
+
+Used to set version in setup.py
+and as an easy way to check which
+version you have installed.
+"""
+
+MAJOR = "0"
+MINOR = "0"
+MAINTAINENCE = "92"
+
+
+def version():
+    """
+    version prints threefives version as a string
+    """
+    return f"{MAJOR}.{MINOR}.{MAINTAINENCE}"
+
+
+def version_number():
+    """
+    version_number returns version as an int.
+    if version() returns 2.3.01
+    version_number will return 2301
+    """
+    return int(f"{MAJOR}{MINOR}{MAINTAINENCE}")
+
+
 # sliding window size
 MEDIA_SLOTS = 10
 # target segment time.
