e93c073c87255fd2c2d642a864cf4c57084a6e86
My name is Adrian <52701496+futzu@users.noreply.github.com>
2022-08-11 19:01:26 +0000

    Added executable script x9k3

--- setup.py
+++ setup.py
@@ -9,14 +9,15 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="x9k3",
     version=x9k3.version(),
-    author="Adrian of DOOM",
+    author="Adrian",
     author_email="spam@iodisco.com",
     description="HLS Segmenter with SCTE-35",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/futzu/x9k3",
     py_modules=["x9k3"],
-    platforms="all",    
+    scripts=['bin/x9k3'],
+    platforms="all",
     install_requires=[
           'threefive',
           'new_reader',
