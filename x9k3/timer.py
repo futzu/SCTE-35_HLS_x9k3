7cad2773cd03835d99fcd82ce4c3937262f16642
Adrian <52701496+futzu@users.noreply.github.com>
2022-12-25 00:28:08 +0000

    Update timer.py

--- x9k3/timer.py
+++ x9k3/timer.py
@@ -7,28 +7,28 @@ class Timer:
         self.end = None
         self.lap_time = None
 
-    def start(self,begin=None):
+    def start(self, begin=None):
         self.begin = begin
         if not self.begin:
             self.begin = time.time()
         self.end = None
         self.lap_time = None
 
-    def stop(self,end=None):
+    def stop(self, end=None):
         self.end = end
         if not self.end:
             self.end = time.time()
         self.lap_time = self.end - self.begin
 
-    def elapsed(self, now =None):
+    def elapsed(self, now=None):
         if not now:
             now = time.time()
         return now - self.begin
 
-    def throttle(self, seg_time,begin=None, end=None):
+    def throttle(self, seg_time, begin=None, end=None):
         self.stop(end)
         diff = round(seg_time - self.lap_time, 2)
         if diff > 0:
             print(f"throttling {diff}")
-            time.sleep(diff/2)
+            time.sleep(diff)
         self.start(begin)
