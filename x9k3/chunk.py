c51ab958f2aa488d908d92121e129d911307ff28
Adrian <52701496+futzu@users.noreply.github.com>
2022-12-26 08:20:15 +0000

    val = None for tags without values

--- x9k3/chunk.py
+++ x9k3/chunk.py
@@ -8,6 +8,7 @@ class Chunk:
     Class to hold hls segment tags
     for a segment.
     """
+
     def __init__(self, name, num):
         self.tags = {}
         self.name = name
@@ -17,7 +18,12 @@ class Chunk:
         """
         get returns the Chunk data formated.
         """
-        this = [f"{k}:{v}" for k, v in self.tags.items()]
+        this = []
+        for k, v in self.tags.items():
+            if v is None:
+                this.append(k)
+            else:
+                this.append(f"{k}:{v}")
         this.append(self.name)
         this.append("")
         this = "\n".join(this)
