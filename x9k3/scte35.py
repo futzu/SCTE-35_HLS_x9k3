b338b56e3e910e141948400dd6e43f1941068e2e
Adrian <52701496+futzu@users.noreply.github.com>
2022-12-26 00:27:27 +0000

    chk_cue_state & mk_cue_state

--- x9k3/scte35.py
+++ x9k3/scte35.py
@@ -18,7 +18,7 @@ class SCTE35:
         self.cue = None
         self.cue_state = None
         self.cue_time = None
-        self.tag_method = self.x_cue
+        self.tag_method = self.x_daterange
         self.break_timer = Timer()
         self.break_duration = None
         self.event_id = 1
@@ -38,22 +38,39 @@ class SCTE35:
         mk_cue_tag routes  hls tag creation
         to the appropriate method.
         """
+        tag = False
         if self.cue:
-            print("self.cue , YES")
-            return self.tag_method()
-        return False
+            if self.is_cue_in(self.cue):
+                self.cue_state = "IN"
+            tag = self.tag_method()
+        return tag
+
+    def chk_cue_state(self):
+        if self.cue_state == "OUT":
+            self.cue_state = "CONT"
+        if self.cue_state == "IN":
+            self.cue_time = None
+            self.cue = None
+            self.cue_state = None
+
+
+    def mk_cue_state(self):
+        if self.is_cue_out(self.cue):
+            self.cue_state= "OUT"
+        if self.is_cue_in(self.cue):
+            self.cue_state= "IN"
 
     def x_cue(self):
         """
         #EXT-X-CUE-( OUT | IN | CONT )
         """
-        print(self.state_of_cue)
-        if self.state_of_cue == "OUT":
+        print(self.cue_state)
+        if self.cue_state == "OUT":
             # self.break_timer = Timer()
             return f"#EXT-X-CUE-OUT:{self.break_duration}"
-        if self.state_of_cue == "IN":
+        if self.cue_state == "IN":
             return "#EXT-X-CUE-IN"
-        if self.state_of_cue == "CONT":
+        if self.cue_state == "CONT":
             return f"#EXT-X-CUE-OUT-CONT:{self.break_timer.elapsed():.6f}/{self.break_duration}"
         return False
 
@@ -62,9 +79,9 @@ class SCTE35:
         #EXT-X-SPLICEPOINT-SCTE35
         """
         base = f"#EXT-X-SPLICEPOINT-SCTE35:{self.cue.encode()}"
-        if self.state_of_cue == "OUT":
+        if self.cue_state == "OUT":
             return f"{base}"
-        if self.state_of_cue == "IN":
+        if self.cue_state == "IN":
             return f"{base}"
         return False
 
@@ -73,11 +90,11 @@ class SCTE35:
         #EXT-X-SCTE35
         """
         base = f'#EXT-X-SCTE35:CUE="{self.cue.encode()}" '
-        if self.state_of_cue == "OUT":
+        if self.cue_state == "OUT":
             return f"{base},CUE-OUT=YES "
-        if self.state_of_cue == "IN":
+        if self.cue_state == "IN":
             return f"{base},CUE-IN=YES "
-        if self.state_of_cue == "CONT":
+        if self.cue_state == "CONT":
             return f"{base},CUE-OUT=CONT"
         return False
 
@@ -91,13 +108,13 @@ class SCTE35:
         if self.break_duration:
             fdur = f",PLANNED-DURATION={self.break_duration}"
 
-        if self.state_of_cue == "OUT":
+        if self.cue_state == "OUT":
             fstart = f',START-DATE="{iso8601}"'
             tag = f"{fbase}{fstart}{fdur},SCTE35-OUT={self.cue.encode_as_hex()}"
             self.event_id += 1
             return tag
 
-        if self.state_of_cue == "IN":
+        if self.cue_state == "IN":
             fstop = f',END-DATE="{iso8601}"'
             tag = f"{fbase}{fstop},SCTE35-IN={self.cue.encode_as_hex()}"
             self.event_id += 1
@@ -110,37 +127,37 @@ class SCTE35:
         to see if it is a cue_out event.
         Returns True for a cue_out event.
         """
-        cmd = cue.command
-        print("IS CUE OUT")
-        if cmd.command_type == 5:
-            if cmd.out_of_network_indicator:
-                if cmd.break_duration:
-                    self.break_duration = cmd.break_duration
-                    return True
+        if cue is not None:
+            cmd = cue.command
+            if cmd.command_type == 5:
+                if cmd.out_of_network_indicator:
+                    if cmd.break_duration:
+                        self.break_duration = cmd.break_duration
+                        return True
 
-        upid_starts = [
-            0x10,
-            0x20,
-            0x22,
-            0x30,
-            0x32,
-            0x34,
-            0x36,
-            0x38,
-            0x3A,
-            0x3C,
-            0x3E,
-            0x44,
-            0x46,
-        ]
-        if cmd.command_type == 6:
-            for dsptr in cue.descriptors:
-                if dsptr.tag == 2:
-                    if dsptr.segmentation_type_id in upid_starts:
-                        if dsptr.segmentation_duration:
-                            self.break_duration = dsptr.segmentation_duration
-                            return True
-        return False
+            upid_starts = [
+                0x10,
+                0x20,
+                0x22,
+                0x30,
+                0x32,
+                0x34,
+                0x36,
+                0x38,
+                0x3A,
+                0x3C,
+                0x3E,
+                0x44,
+                0x46,
+            ]
+            if cmd.command_type == 6:
+                for dsptr in cue.descriptors:
+                    if dsptr.tag == 2:
+                        if dsptr.segmentation_type_id in upid_starts:
+                            if dsptr.segmentation_duration:
+                                self.break_duration = dsptr.segmentation_duration
+                                return True
+            return False
 
     def is_cue_in(self, cue):
         """
@@ -148,31 +165,31 @@ class SCTE35:
         to see if it is a cue_in event.
         Returns True for a cue_in event.
         """
-        cmd = cue.command
-        if cmd.command_type == 5:
-            if not cmd.out_of_network_indicator:
-                if self.break_timer.elapsed() >= self.break_duration:
+        if cue is not None:
+            cmd = cue.command
+            if cmd.command_type == 5:
+                if not cmd.out_of_network_indicator:
                     return True
 
-        upid_stops = [
-            0x11,
-            0x21,
-            0x21,
-            0x23,
-            0x33,
-            0x35,
-            0x37,
-            0x39,
-            0x3B,
-            0x3D,
-            0x3F,
-            0x45,
-            0x47,
-        ]
-        if cmd.command_type == 6:
-            for dsptr in cue.descriptors:
-                if dsptr.tag == 2:
-                    if dsptr.segmentation_type_id in upid_stops:
-                        if self.break_timer.elapsed() >= self.break_duration:
-                            return True
+            upid_stops = [
+                0x11,
+                0x21,
+                0x21,
+                0x23,
+                0x33,
+                0x35,
+                0x37,
+                0x39,
+                0x3B,
+                0x3D,
+                0x3F,
+                0x45,
+                0x47,
+            ]
+            if cmd.command_type == 6:
+                for dsptr in cue.descriptors:
+                    if dsptr.tag == 2:
+                        if dsptr.segmentation_type_id in upid_stops:
+                            if self.break_timer.elapsed() >= self.break_duration:
+                                return True
         return False
