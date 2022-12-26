561c0193f399d827d4f95ab7f867f4a00211b3e1
Adrian <52701496+futzu@users.noreply.github.com>
2022-12-26 00:25:35 +0000

    moved several methods to scte35.py

--- x9k3/expx9k3.py
+++ x9k3/expx9k3.py
@@ -16,7 +16,7 @@ class ExpX9K3(Stream):
         self.video = tsdata
         self.active_segment = io.BytesIO()
         self.iframer = IFramer()
-        self.window = SlidingWindow(50)
+        self.window = SlidingWindow(500)
         self.scte35 = SCTE35()
         self.packet_size = 188
         self.seconds = 2
@@ -45,6 +45,7 @@ class ExpX9K3(Stream):
         return "\n".join(head)
 
     def add_cue_tag(self, chunk):
+        print("adding cue tag")
         tag = self.scte35.mk_cue_tag()
         print(tag)
         if tag:
@@ -59,15 +60,16 @@ class ExpX9K3(Stream):
     def _write_segment(self):
         seg_name = f"seg{self.segnum}.ts"
         seg_time = self.next_start - self.started
-
         with open(seg_name, "wb") as seg:
             seg.write(self.active_segment.getbuffer())
         chunk = Chunk(seg_name, self.segnum)
-        # self.add_cue_tag(chunk)
+        self.add_cue_tag(chunk)
         chunk.add_tag("#EXTINF", f"{seg_time:.6f},")
-        print(chunk.get())
+        #chunk.add_tag("#EXT-X-FU",f"{self.started}-{self.next_start} ")
         self.window.slide_panes(chunk)
         self._write_m3u8()
+        self._start_next_start()
+        self.scte35.chk_cue_state()
 
     def _write_m3u8(self):
         with open(self.m3u8, "w+") as m3u8:
@@ -78,22 +80,56 @@ class ExpX9K3(Stream):
                 m3u8.write("#EXT-X-ENDLIST")
         self.active_segment = io.BytesIO()
 
+    def _start_next_start(self,pts=None):
+        if pts is not None:
+            self.started = pts
+        else:
+            self.started = self.next_start
+        self.next_start = self.started + self.seconds
+
+    def slice_check(self,now):
+        if self.scte35.cue_time:
+            print("CUE: ", self.scte35.cue_time)
+            if now >= self.scte35.cue_time:
+                self.next_start= self.scte35.cue_time
+                self._write_segment()
+                self.scte35.break_timer.start()
+                self.scte35.cue_time=None
+                self.scte35.mk_cue_state()
+        else:
+            if  now >= self.next_start:
+                self.next_start = now
+                self._write_segment()        
+
+    def _chk_cue(self, pid):
+        """
+        _chk_cue checks for SCTE-35 cues
+        and inserts a tag at the time
+        the cue is received.
+        """
+        if self.scte35.cue:
+            if "pts_time" in self.scte35.cue.command.get():
+                self.scte35.cue_time = self.scte35.cue.command.pts_time
+
+            else:
+                self.scte35.cue_time = self.pid2pts(pid)
+
+    def _parse_scte35(self, pkt, pid):
+        cue = super()._parse_scte35(pkt,pid)
+        if cue:
+            self.scte35.cue = cue
+            self._chk_cue(pid)
+        return cue
+
     def _parse(self, pkt):
         super()._parse(pkt)
         pkt_pid = self._parse_pid(pkt[1], pkt[2])
-        pkt_pts = self.pid2pts(pkt_pid)
-        # print(pkt_pts)
+        now = self.pid2pts(pkt_pid)
         if not self.started:
-            self.started = pkt_pts
-        if not self.next_start:
-            self.next_start = self.started + self.seconds
+            self._start_next_start(pts=now)
         i_pts = self.iframer.parse(pkt)
         if i_pts:
-            if pkt_pts >= self.next_start or self.scte35.cue_time:
-                self.next_start = pkt_pts
-                self._write_segment()
-                self.started = self.next_start
-                self.next_start = self.started + self.seconds
+            self.slice_check(now)
         self.active_segment.write(pkt)
 
     def slicer(self):
