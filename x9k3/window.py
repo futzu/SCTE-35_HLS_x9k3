75f984020d6eb72ecc163088a742ba0f78c80074
Adrian <52701496+futzu@users.noreply.github.com>
2024-01-21 02:10:06 -0500

    pylint

--- x9k3/window.py
+++ x9k3/window.py
@@ -1,3 +1,16 @@
+"""
+x9k3
+
+window.py
+
+home of the SlidingWindow class.
+"""
+
+from collections import deque
+import os
+from pathlib import Path
+from threefive import print2
+
 
 class SlidingWindow:
     """
@@ -40,56 +53,3 @@ class SlidingWindow:
             self.push_pane(a_pane)
         if len(self.panes) > self.size:
             self.popleft_pane()
-
-
-class Timer:
-    """
-    Timer class instances are used for
-    segment duration, and live throttling.
-    """
-
-    def __init__(self):
-        self.started = time.time()
-        self.begin = None
-        self.end = None
-        self.lap_time = None
-
-    def start(self, begin=None):
-        """
-        start starts the timer
-        """
-        self.begin = begin
-        if not self.begin:
-            self.begin = time.time()
-        self.end = None
-        self.lap_time = None
-
-    def stop(self, end=None):
-        """
-        stop stops the timer
-        """
-        self.end = end
-        if not self.end:
-            self.end = time.time()
-        self.lap_time = self.end - self.begin
-
-    def elapsed(self, now=None):
-        """
-        elapsed returns the elapsed time
-        """
-        if not now:
-            now = time.time()
-        return now - self.started
-
-    def throttle(self, seg_time, begin=None, end=None):
-        """
-        throttle is called to slow segment creation
-        to simulate live streaming.
-        """
-        self.stop(end)
-        diff = round((seg_time - self.lap_time), 2)
-        if diff > 0:
-            print2(f"throttling {diff}")
-            time.sleep(diff)
-        self.start(begin)
-
